cb962ff06fd04c8a58746a2911c4fb16d22391eb
浜崎優一 <hamasakiyuichi@hamazakiyuuichinoMacBook-Pro.local>
2020-09-14 06:24:49 +0900

    improve headers

--- app/assets/javascripts/js/custom.js
+++ app/assets/javascripts/js/custom.js
@@ -1,26 +1,3 @@
-//============================== header =========================
-
-jQuery(document).ready(function($) {
-
-    var navbar = $('.navbar-main'),
-        distance = navbar.offset().top,
-        $window = $(window);
-
-      $window.scroll(function() {
-        if(($window.scrollTop() >= distance) && ($(".navbar-default").hasClass("navbar-main")))
-          {
-              navbar.removeClass('navbar-fixed-top').addClass('navbar-fixed-top');
-              $("body").addClass("padding-top");
-              $(".topBar").css("display","none");
-          } else {
-              navbar.removeClass('navbar-fixed-top');
-              $("body").removeClass("padding-top");
-              $(".topBar").css("display","block");
-          }
-      });
-
-});
-
 //============================== ALL DROPDOWN ON HOVER =========================
 jQuery(document).ready(function(){
     $('.dropdown').hover(function() {
