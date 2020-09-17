aa2239b42da5c5f94af56d6307e285a30eb9dd81
浜崎優一 <hamasakiyuichi@hamazakiyuuichinoMacBook-Pro.local>
2020-09-17 11:05:55 +0900

    try to add assets controller

---
+++ app/controllers/assets_controller.rb
@@ -0,0 +1,33 @@
+class AssetsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @store = Address.find(params[:id])
+
+    if @store.assets.create(asset_params)
+      flash[:success] = "photo was successfully save."
+      redirect_to parlor_path(@store.id)
+    else
+      @title = @store.name
+      @parlor_new = Parlor.new
+      @parlor = @store.parlor
+      @address = @store
+      flash.now[:danger] = "Could not save photo"
+      render 'parlors/edit'
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  # Only allow a trusted parameter "white list" through.
+    def asset_params
+      params.require(:asset).permit(
+        :photo)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :parlors do
     resources :addresses, only: [:create, :update, :destroy]
+    resources :assets, only: [:create, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
